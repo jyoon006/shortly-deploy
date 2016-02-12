459bb51696d2cc197031ab646e6c590f463abe5e
Jisoo Yoon <jyoon006@gmail.com>
2016-02-11 21:32:38 -0500

    db functionality working correctly

--- app/models/link.js
+++ app/models/link.js
@@ -1,26 +1,7 @@
-// var db = require('../config');
+
 var crypto = require('crypto');
 var mongoose = require('mongoose');
-// var environment = process.env.NODE_ENV || 'development';
 
-// if (environment === 'development') {
-
-//   var Link = db.Model.extend({
-//     tableName: 'urls',
-//     hasTimestamps: true,
-//     defaults: {
-//       visits: 0
-//     },
-//     initialize: function(){
-//       this.on('creating', function(model, attrs, options){
-//         var shasum = crypto.createHash('sha1');
-//         shasum.update(model.get('url'));
-//         model.set('code', shasum.digest('hex').slice(0, 5));
-//       });
-//     }
-//   });
-
-// }
   //add functions
   var Schema = mongoose.Schema;
   var urlSchema = new Schema ({
